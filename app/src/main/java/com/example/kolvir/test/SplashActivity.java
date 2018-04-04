14aabd232eb9b13bba1e5d7424dfab9067b930f2
PixelGuy101 <kulikov-me@yandex.ru>
2018-04-04 17:06:35 +0300

    Commit 04.04.2018 17:06(Add SplashScreen = work(100%))

--- app/src/main/java/com/example/kolvir/test/SplashActivity.java
+++ app/src/main/java/com/example/kolvir/test/SplashActivity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,19 +18,29 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class SplashActivity extends AppCompatActivity {
-
+private ImageView iv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = new Intent(this, MainActivity.class);
-        try {
-            TimeUnit.SECONDS.sleep(2);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+        iv = (ImageView) findViewById(R.id.splash_tree);
+        Animation tree_anim = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        iv.startAnimation(tree_anim);
+        final Intent intent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 
 
