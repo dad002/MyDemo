b61bd7aee74a62171409cc7c1379ed32075a402d
PixelGuy101 <kulikov-me@yandex.ru>
2018-04-08 12:20:33 +0300

    Commit 08.04.2018(Add TextView on About_us_screen = work(80%))

--- app/src/main/java/com/example/kolvir/test/AboutUsActivity.java
+++ app/src/main/java/com/example/kolvir/test/AboutUsActivity.java
@@ -3,17 +3,37 @@ package com.example.kolvir.test;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by kulik on 05.04.2018.
  */
 
 public class AboutUsActivity extends AppCompatActivity {
-
+    TextView tvArtemy;
+    TextView tvMikhail;
+    TextView tvThird;
+    TextView tvRights;
+    ImageView ivLogo;
+    boolean isVisible = true;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_about_us);
+        tvArtemy = (TextView) findViewById(R.id.textView4);
+        tvMikhail = (TextView) findViewById(R.id.textView5);
+        tvThird = (TextView) findViewById(R.id.textView6);
+        tvRights = (TextView) findViewById(R.id.textView3);
+        ivLogo = (ImageView) findViewById(R.id.imageView2);
+        Animation text_anim = AnimationUtils.loadAnimation(this, R.anim.about_us_anim);
+        tvArtemy.startAnimation(text_anim);
+        tvMikhail.startAnimation(text_anim);
+        tvThird.startAnimation(text_anim);
+        tvRights.startAnimation(text_anim);
+        ivLogo.startAnimation(text_anim);
     }
 }
--- app/src/main/java/com/example/kolvir/test/SplashActivity.java
+++ app/src/main/java/com/example/kolvir/test/SplashActivity.java
@@ -59,5 +59,6 @@ public class SplashActivity extends AppCompatActivity {
             }
         };
         timer.start();
+        Log.i("RVINFO", "onCreate_splash");
     }
 }
